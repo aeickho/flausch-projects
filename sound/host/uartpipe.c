df120bd577fe840a9789b5d364bd7c7db87451ea
Alexander Eickhoff <alex@linux-ig0c.site>
2012-10-14 15:51:28 +0200

    +set io speed

--- sound/host/uartpipe.c
+++ sound/host/uartpipe.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/fcntl.h>
+#include <termios.h>
 
 #define CHUNKSIZE 192
 
 int main(int argc, char** argv) {
 	FILE* tty;
+	int ttyId;
 	int c;
 	char val;
 	char buf[CHUNKSIZE];
+	
+	struct termios termOptions;
+
 
 	if(argc < 2) {
 		fprintf(stderr, "you need to give the tty device as first argument.\n");
 		exit(1);
 	}
+
+	ttyId = open (argv[1], O_RDWR | O_NOCTTY );
+	if (ttyId < 0) {
+	        perror ("opening TTY");
+		exit (2);
+	}
+	tcgetattr( ttyId, &termOptions );
+	cfsetispeed( &termOptions, B921600 );
+	cfsetospeed( &termOptions, B921600 );
+	tcsetattr( ttyId, TCSANOW, &termOptions );
+	close(ttyId);                               
+	                               
 	
 	tty = fopen(argv[1], "r+");
 	if(tty == NULL) {
